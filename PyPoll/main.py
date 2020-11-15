0b2ca1493d547c961705926835f71a5547fde30f
perkins228 <perkins.228@gmail.com>
2020-11-15 00:23:15 -0500

    corrected upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -33,7 +33,15 @@ with open(filepath) as csvfile:
     li_per = round(li_votes/total_votes,2)
     otooley_per = round(otooley_votes/total_votes,2)
 
-
+    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+    if winner == khan_votes:
+        winner_is = "Khan"
+    elif winner == correy_votes:
+        winner_is = "Correy"
+    elif winner == li_votes:
+        winner_is = "Li"
+    else:
+        winner_is = "O'Tooley"
 
 
 print("Election Results")
@@ -44,3 +52,22 @@ print(f"Khan: {khan_per:.3%} ({khan_votes})")
 print(f"Correy: {correy_per:3%} ({correy_votes})")
 print(f"Li: {li_per:.3%}% ({li_votes})")
 print(f"O'Tooley: {otooley_per:.3%}% ({otooley_votes})")
+print("----------------------------------")
+print(f"Winner: {winner_is}")
+print("----------------------------------")
+
+textfile = os.path.join("./Resources/election_data.text")
+
+with open(textfile, "w") as txtfile:
+
+txtfile.write("Election Results/n")
+txtfile.write("----------------------------------/n")
+txtfile.write(f"Total Votes: {total_votes}/n")
+txtfile.write("----------------------------------/n")
+txtfile.writef"Khan: {khan_per:.3%} ({khan_votes})/n")
+txtfile.write(f"Correy: {correy_per:3%} ({correy_votes})/n")
+txtfile.write(f"Li: {li_per:.3%}% ({li_votes})/n")
+txtfile.write(f"O'Tooley: {otooley_per:.3%}% ({otooley_votes})/n")
+txtfile.write("----------------------------------/n")
+txtfile.write(f"Winner: {winner_is}")
+txtfile.write("----------------------------------/n")
