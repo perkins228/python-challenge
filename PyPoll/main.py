cfb8f3ceba8bfddccbc07a6122c7bfed7a1ad378
perkins228 <perkins.228@gmail.com>
2020-11-15 00:10:59 -0500

    new issue found

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -28,13 +28,19 @@ with open(filepath) as csvfile:
         if row [2] == "O'Tooley":
             otooley_votes = otooley_votes + 1
 
+    khan_per = round(khan_votes/total_votes,2)
+    correy_per = round(correy_votes/total_votes,2)
+    li_per = round(li_votes/total_votes,2)
+    otooley_per = round(otooley_votes/total_votes,2)
 
 
 
 
-
-
-print(khan_votes)
-print(correy_votes)
-print(li_votes)
-print(otooley_votes)
+print("Election Results")
+print("----------------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------------")
+print(f"Khan: {khan_per:.3%} ({khan_votes})")
+print(f"Correy: {correy_per:3%} ({correy_votes})")
+print(f"Li: {li_per:.3%}% ({li_votes})")
+print(f"O'Tooley: {otooley_per:.3%}% ({otooley_votes})")
