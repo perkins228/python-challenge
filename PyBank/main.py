a6c6198f2547afedd280d68dd3689ee8c721d989
perkins228 <perkins.228@gmail.com>
2020-11-14 22:06:15 -0500

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,19 +2,29 @@ import os
 import csv
 
 #import file
-filepath = os.path.join("/PyBank/Resources/budget_data.csv")
+filepath = os.path.join("./Resources/budget_data.csv")
 total_months = 0
 revenue = 0
+change = []
+dates = []
 
 
 
 with open(filepath) as csvfile:
     bankcsv = csv.reader(csvfile, delimiter = ",")
     next(bankcsv)
-    total_months = total_months + 1
-    revenue = revenue + 1
+    prior_rev = 0
 
     for row in bankcsv:
         total_months = total_months + 1
+        prior_rev = int(row[1])
         revenue = revenue + int(row[1])
-        print(revenue)
+        change.append(int(row[1]) - prior_rev )
+        
+
+
+
+
+print(f"Total Months: {total_months}")
+print(f"Total: ${revenue}")
+print(change)
